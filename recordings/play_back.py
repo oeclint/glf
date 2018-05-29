9182bbde20dce0b25ee97e1b8101ca69eb2f8e21
Clinton, Omar <omar.clinton@pnnl.gov>
2018-05-29 10:12:10 -0700

    allow playback to play all vids

--- recordings/play_back.py
+++ recordings/play_back.py
@@ -1,15 +1,17 @@
 import retro
+import glob
 
-movie = retro.Movie('SonicTheHedgehog-Genesis-LabyrinthZone.Act1-0000.bk2')
-movie.step()
+for f in glob.glob('.*bk2'):
+    movie = retro.Movie(f)
+    movie.step()
 
-env = retro.make(game=movie.get_game(), state=retro.STATE_NONE, use_restricted_actions=retro.ACTIONS_ALL)
-env.initial_state = movie.get_state()
-env.reset()
+    env = retro.make(game=movie.get_game(), state=retro.STATE_NONE, use_restricted_actions=retro.ACTIONS_ALL)
+    env.initial_state = movie.get_state()
+    env.reset()
 
-while movie.step():
-    keys = []
-    for i in range(env.NUM_BUTTONS):
-        keys.append(movie.get_key(i))
-    _obs, _rew, _done, _info = env.step(keys)
-    env.render()
+    while movie.step():
+        keys = []
+        for i in range(env.NUM_BUTTONS):
+            keys.append(movie.get_key(i))
+        _obs, _rew, _done, _info = env.step(keys)
+        env.render()
