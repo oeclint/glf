ad543233f61f84da7fae687b6169311315f11749
Clinton, Omar <omar.clinton@pnnl.gov>
2018-06-01 14:52:47 -0700

    removing batch norm

--- train_sonic.py
+++ train_sonic.py
@@ -66,7 +66,6 @@ class ConvNet(nn.Module):
         self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
         self.bn3 = nn.BatchNorm2d(32)
         self.fc1 = nn.Linear(29600, 512)
-        self.bn4 = nn.BatchNorm1d(512)
         self.fc2 = nn.Linear(512, 10)
     
     def forward(self, x):
@@ -74,7 +73,7 @@ class ConvNet(nn.Module):
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
         x = x.view(x.size(0), -1)
-        x = F.relu(self.bn4(self.fc1(x)))
+        x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
 
