4d088e30bf16b977ffeda3c30abdaaf7b0060e47
Clinton, Omar <omar.clinton@pnnl.gov>
2018-06-01 12:01:54 -0700

    updating CNN

--- train_sonic.py
+++ train_sonic.py
@@ -65,15 +65,16 @@ class ConvNet(nn.Module):
         self.bn2 = nn.BatchNorm2d(32)
         self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
         self.bn3 = nn.BatchNorm2d(32)
-        self.fc1 = nn.Linear(29600, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.fc1 = nn.Linear(29600, 512)
+        self.bn4 = nn.BatchNorm1d(512)
+        self.fc2 = nn.Linear(512, 10)
     
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
         x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
+        x = F.relu(self.bn4(self.fc1(x)))
         x = self.fc2(x)
         return x
 
