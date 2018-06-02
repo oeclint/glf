c9f247b1f7cfe8ff4736c9f7ed4de44ea4a8678a
Clinton, Omar <omar.clinton@pnnl.gov>
2018-06-01 19:34:10 -0700

    new reward func

--- recordings/play_back.py
+++ recordings/play_back.py
@@ -1,7 +1,7 @@
 import retro
 import glob
 
-for f in glob.glob('*000.bk2'):
+for f in glob.glob('*006.bk2'):
     movie = retro.Movie(f)
     movie.step()
 
--- sonic_util.py
+++ sonic_util.py
@@ -1,5 +1,6 @@
 import gym
 import numpy as np
+from collections import deque
 
 def process_obs(obs):
     """
@@ -39,19 +40,24 @@ class SonicEnvWrapper(gym.Wrapper):
     """
     def __init__(self, env):
         super(SonicEnvWrapper, self).__init__(env)
-        self._cur_x = 0
-        self._max_x = 0
+#        self._cur_x = 0
+#        self._max_x = 0
+        self.rewards = deque(maxlen=10)
 
     def reset(self, **kwargs):
-        self._cur_x = 0
-        self._max_x = 0
+#        self._cur_x = 0
+#        self._max_x = 0
         obs = self.env.reset(**kwargs)
         return process_obs(obs)
 
     def step(self, action):
         obs, rew, done, info = self.env.step(action)
-        if rew < 0:
-            rew = rew * 0.4
+        self.rewards.append(rew)
+        if len(self.rewards)==10:
+            if all([rew_<0.01 for rew_ in self.rewards]):
+                rew = -20
+            elif rew < 0:
+                rew = rew * 0.33
  #       self._cur_x += rew
  #       rew = max(0.0, self._cur_x - self._max_x)
  #       self._max_x = max(self._max_x, self._cur_x)
