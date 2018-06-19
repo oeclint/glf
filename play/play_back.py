4fed6d2105dd32d243a46733f3e9ac64fed78dbe
Clinton, Omar <omar.clinton@pnnl.gov>
2018-06-19 15:14:36 -0700

    filtered human data to exclude actions past done

--- play/play_back.py
+++ play/play_back.py
@@ -5,14 +5,19 @@ from retro_contest.local import make as contest_make
 from retro import make
 import argparse
 from glf.common.sonic_util import SonicActions
+from glf.common.parse import to_json
 
 class PlayBack(object):
 
-    def __init__(self, game, state, scenario, root='human'):
+    def __init__(self, game, state, scenario, root='human',maker=None):
         self.path = os.path.join(root,game,scenario)
         self.game = game
         self.state = state
         self.scenario = scenario
+        if maker is None:
+            self.make = make
+        else:
+            self.make = maker
 
     def play_bk2(self, render=False, write=True):
 
@@ -51,12 +56,12 @@ class PlayBack(object):
             with open('{}-{}.json'.format(self.game,self.state), 'w') as outfile:
                  outfile.write(to_json(keysdict))
 
-    def play_json(self, maker, render=True):
+    def play_json(self, render=True):
 
         with open(os.path.join(self.path,'{}-{}.json'.format(self.game,self.state))) as f:
             data = json.load(f)
 
-        env = maker(game=self.game, state=self.state, scenario=self.scenario)
+        env = self.make(game=self.game, state=self.state, scenario=self.scenario)
         for ep in data:
             obs = env.reset()
             actions = SonicActions.from_sonic_config(data[ep])
@@ -65,6 +70,40 @@ class PlayBack(object):
                 env.render()
                 if done:
                     break
+
+    def filter_json(self):
+
+        for fi in glob.glob(os.path.join(self.path,'{}-{}.json'.format(self.game,self.state))):
+
+            base = os.path.basename(fi)
+            base = os.path.splitext(base)[0]
+            splits = base.split('-')
+            state = splits[-1]
+            game = '-'.join([splits[0],splits[1]])
+
+            filtered_data = {}
+
+            with open(fi) as f:
+                data = json.load(f)
+
+            env = self.make(game=game, state=state, scenario=self.scenario)
+            for ep in data:
+                filtered_actions = []
+                obs = env.reset()
+                for action in data[ep]:
+                    filtered_actions.append(action)
+                    obs, rew, done, info = env.step(action)
+                    if done:
+                        l = len(filtered_data)
+                        filtered_data[l] = filtered_actions
+                        break
+
+            with open(fi,'w') as outfile:
+                outfile.write(to_json(filtered_data))
+
+            env.close()
+
+
 
     def actions(self):
         with open(os.path.join(self.path,'{}-{}.json'.format(self.game,self.state))) as f:
@@ -84,4 +123,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
     p = PlayBack(game=args.game,state=args.state,scenario=args.scenario)
   #  p.play_bk2()
-    p.play_json(make)
+  #  p.play_json()
+    p.filter_json()
